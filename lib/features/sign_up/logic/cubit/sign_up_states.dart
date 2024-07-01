057b69474c5cc377dc3bc171fa7e766831eb536f
DESKTOP-GIGKCG7\ahmed <ahmedadel121216@gmail.com>
2024-07-01 04:07:02 +0300

    -add sign_up_cubit -add user_create_model

---
+++ lib/features/sign_up/logic/cubit/sign_up_states.dart
@@ -0,0 +1,15 @@
+abstract class SignUpStates {}
+
+class InitialSignUpState extends SignUpStates {}
+
+class ChangePasswordVisibility extends SignUpStates {}
+
+class UserSignUpLoadingState extends SignUpStates {}
+
+class UserSignUpSuccessState extends SignUpStates {}
+
+class UserSignUpErrorState extends SignUpStates {}
+
+class UserCreateSuccessState extends SignUpStates {}
+
+class UserCreateErrorState extends SignUpStates {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sawa_chat/core/routing/app_router.dart';
+import 'features/bloc/bloc_observer.dart';
 import 'sawa_app.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await ScreenUtil.ensureScreenSize();
+  Bloc.observer = MyBlocObserver();
   runApp(SawaApp(
     appRouter: AppRouter(),
   ));
