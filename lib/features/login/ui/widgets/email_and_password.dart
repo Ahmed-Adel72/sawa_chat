c5d8f1495e4df9dc3da66d490c70a56832e4371c
DESKTOP-GIGKCG7\ahmed <ahmedadel121216@gmail.com>
2024-07-02 20:29:48 +0300

    - added login cubit - added login states

--- lib/features/login/ui/widgets/email_and_password.dart
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sawa_chat/core/widgets/app_text_form_field.dart';
+import 'package:sawa_chat/features/login/logic/cubit/login_cubit.dart';
 
 // ignore: must_be_immutable
 class EmailAndPassword extends StatelessWidget {
   EmailAndPassword({super.key});
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
+    var cubit = LoginCubit.get(context);
     return Column(
       children: [
         Container(
           height: 280.h,
         ),
         AppTextFormField(
-          controller: emailController,
+          controller: cubit.emailController,
           hintText: "Email",
           isObscureText: false,
           prefixIcon: const Icon(Icons.email_outlined),
@@ -31,10 +30,19 @@ class EmailAndPassword extends StatelessWidget {
           height: 28.h,
         ),
         AppTextFormField(
-          controller: passwordController,
+          controller: cubit.passwordController,
           hintText: "Password",
-          isObscureText: true,
+          isObscureText: cubit.isPasswordObscure,
           prefixIcon: const Icon(Icons.lock_outline),
+          suffixIcon: IconButton(
+            onPressed: () {
+              cubit.changePasswordVisibility();
+            },
+            icon: Icon(
+              cubit.suffix,
+              size: 22,
+            ),
+          ),
           validator: (value) {
             if (value == null || value.isEmpty) {
               return 'Please enter a vaild password';
